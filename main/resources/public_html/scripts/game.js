80fb67d969c930a3a203363c29dc16601204a18e
Lukas K <karras.lukas@web.de>
2023-01-04 19:42:29 +0100

    Added scale and draw functionality for the canvases. Added a heart icon.

--- main/resources/public_html/scripts/game.js
+++ main/resources/public_html/scripts/game.js
@@ -1,8 +1,86 @@
+var canvas;
+var canvas_enemies;
+var width;
+var width_enemies;
+var num_rows = 10;
+window.onload = windowLoaded;
+
+function windowLoaded(){
+    // TODO connectToWebsocket();
+
+    canvas = document.querySelector(".own_game");
+    canvas_enemies = document.querySelectorAll(".enemy");
+
+    windowResized();
+}
+
+window.addEventListener("resize", windowResized);
+
+function windowResized(evt){
+    width = window.innerWidth * 0.34;
+    canvas.setAttribute("height", width);
+    canvas.setAttribute("width", width);
+
+    width_enemies = width * 0.2;
+    for(let c of canvas_enemies){
+        c.setAttribute("height", width_enemies);
+        c.setAttribute("width", width_enemies);
+    }
+
+    drawCanvas();
+}
+
+function drawCanvas(){
+    drawGrid();
+    drawSnakes();
+    updateInfobar();
+}
+
+function drawGrid(){
+    // draw own game field
+    let ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    // drawing the grid
+    for (let i = 0; i < width; i+= (width-1)/num_rows) {
+        ctx.moveTo(0, i);
+        ctx.lineTo(width, i);
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, width);
+    }
+    ctx.stroke();
+    ctx.closePath();
+
+
+    // draw Enemy game fields
+    for(cv of canvas_enemies){
+        ctx = cv.getContext("2d");
+        ctx.beginPath();
+        // drawing the grid
+        for (let i = 0; i < width_enemies; i+= (width_enemies-1)/num_rows) {
+            ctx.moveTo(0, i);
+            ctx.lineTo(width_enemies, i);
+            ctx.moveTo(i, 0);
+            ctx.lineTo(i, width_enemies);
+        }
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
+
+function drawSnakes(){
+    // TODO
+}
+
+function updateInfobar(){
+    // TODO
+}
+
+/*
 let socket = new WebSocket("ws://localhost:5001");
+
 
 // adding keydown EventListener for movement input of the User
 document.addEventListener('keydown', keyInput);
-
 
 function keyInput(evt){
     // whitelist of keys to be sent
@@ -26,3 +104,11 @@ function keyInput(evt){
         socket.send(key_mapping[evt.key]);
     }
 }
+
+socket.onmessage = parseMessage;
+
+function parseMessage(msg){
+
+    // TODO
+}
+*/
