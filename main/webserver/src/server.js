eb96804123a59916ddfde272ca941164a2f383cf
Lukas Karras <karras.lukas@web.de>
2023-01-09 10:55:54 +0100

    moved .ejs files from resources to webserver

--- main/webserver/src/server.js
+++ main/webserver/src/server.js
@@ -6,7 +6,7 @@ server.use(express.static(path.join(__dirname+"/../../resources/public_html", ".
 server.use(express.urlencoded({ extended: false, limit: "1mb"}));
 server.use(express.json());
 server.set("view engine", "ejs");
-server.set('views', path.join(__dirname, "../../resources/public_html/html"));
+server.set('views', path.join(__dirname, "/views"));
 
 server.get('/', function (req, res){
     res.render("index");
