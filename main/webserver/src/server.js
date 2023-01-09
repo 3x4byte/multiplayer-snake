ca8831a2adcefb965209dc93958f67ee344dd60b
Lukas Karras <karras.lukas@web.de>
2023-01-09 09:52:43 +0100

    using ejs to split up the '.html' files

--- main/webserver/src/server.js
+++ main/webserver/src/server.js
@@ -6,6 +6,12 @@ server.use(express.static(path.join(__dirname+"/../../resources/public_html", ".
 server.use(express.urlencoded({ extended: false, limit: "1mb"}));
 server.use(express.json());
 server.set("view engine", "ejs");
+server.set('views', path.join(__dirname, "../../resources/public_html/html"));
+
+server.get('/', function (req, res){
+    res.render("index");
+})
+
 server.listen(portnumber, function(){
     console.log(`listening at port: ${portnumber}`)
 });
